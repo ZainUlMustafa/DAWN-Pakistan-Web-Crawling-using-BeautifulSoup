3b8a433a923a7e5ea7edf676ec87210111e09f54
ZainUlMustafa <zain2984@gmail.com>
2018-12-02 02:33:16 +0500

    with simple senti analysis
    run this for multi day extraction

--- data.py
+++ data.py
@@ -1,22 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
 from datetime import datetime,timedelta
+from textblob import TextBlob
 
 dataset = []
-day_count = 50
-for i in range(0,day_count):
+sentiment_index = 0.0
+
+for i in range(0,500):
 	date = str(datetime.today().date() - timedelta(days=i))
 
-	url = 'https://www.dawn.com/newspaper/business/'+date
-	res = requests.get(url)
-	soup = BeautifulSoup(res.text, 'html.parser')
+	try:
+		url = 'https://www.dawn.com/newspaper/business/'+date
+		res = requests.get(url)
 
-	title_cont = soup.find_all('div', class_='col-sm-10')
+		soup = BeautifulSoup(res.text, 'html.parser')
 
-	for title in title_cont:
-		data = date+','+title.h2.text
-		dataset.append(data)
-	print(date,'-> extracted')
+		title_cont = soup.find_all('div', class_='col-sm-10')
+
+		for title in title_cont:
+			text = title.h2.text
+			if text[-1] == '\n':
+				text = text[:-1]
+			blob = TextBlob(str(text))
+			for sentence in blob.sentences:
+				sentiment_index = sentence.sentiment.polarity
+
+			data = date+','+text+','+str(round(sentiment_index,3))
+			dataset.append(data)
+		print(i,':',date,'-> extracted')
+	except:
+		print(i,':',date,'-> unextracted')
 
 print(type(dataset[0]))
 with open('dataset.txt', 'w') as f:
